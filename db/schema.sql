230644ced25824c7e244b0bf27a82a960fd348f6
Mike L <mike.levy.rva@gmail.com>
2022-08-14 17:28:35 -0400

    added db folder, sql files, and schema table info

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department (
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE employee_role (
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(6, 0) NOT NULL,
+    department_id INTEGER NOT NULL,
+);
+
+
+CREATE TABLE employee (
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL, 
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
+);
+
+
