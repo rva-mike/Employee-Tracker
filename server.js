b03f02f6401b2e62c28ad6718d3a4d467c5aebc8
Mike L <mike.levy.rva@gmail.com>
2022-08-14 20:41:13 -0400

    added 'addEmployee' function

--- server.js
+++ server.js
@@ -160,3 +160,41 @@ function addRole() {
         });
       });
   }
+
+
+
+  function addEmployee() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What's the first name of the employee?",
+          name: "eeFirstName"
+        },
+        {
+          type: "input",
+          message: "What's the last name of the employee?",
+          name: "eeLastName"
+        },
+        {
+          type: "input",
+          message: "What is the employee's role id number?",
+          name: "roleID"
+        },
+        {
+          type: "input",
+          message: "What is the manager id number?",
+          name: "managerID"
+        }
+      ])
+      .then(function(answer) {
+  
+        
+        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answer.eeFirstName, answer.eeLastName, answer.roleID, answer.managerID], function(err, res) {
+          if (err) throw err;
+          console.table(res);
+          startPrompt();
+        });
+      });
+  }
+
