3dafad3054fac0ec30bde0db9917ed0fc516a8ec
Mike L <mike.levy.rva@gmail.com>
2022-08-14 20:33:21 -0400

    added beginnings to inquirer prompt function

--- server.js
+++ server.js
@@ -18,4 +18,55 @@ const connection = mysql.createConnection({
     console.log('Employee Manager');
     console.log('****************');
 
+    startPrompt()
+
   });
+
+
+  function startPrompt() {
+    inquirer
+      .prompt({
+        type: "list",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add department",
+          "Add role",
+          "Add employee",
+          "Update employee role",
+          "Quit"
+        ],
+        message: "What would you like to do?",
+        name: "option"
+      })
+      .then(function(result) {
+        console.log("You entered: " + result.option);
+  
+        switch (result.option) {
+          case "View all departments":
+            viewDepartment();
+            break;
+          case "View all roles":
+            viewRoles();
+            break;
+          case "View all employees":
+            viewEmployees();
+            break;
+          case "Add department":
+            addDepartment();
+            break;
+          case "Add role":
+            addRole();
+            break;
+          case "Add employee":
+            addEmployee();
+            break;
+          case "Update employee role":
+            updateEmployee();
+            break;
+          default:
+            quit();
+        }
+      });
+  }
