7e72cea9649c208837ec93cd078ca4d2cf081d80
Mike L <mike.levy.rva@gmail.com>
2022-08-14 20:44:00 -0400

    added 'quit' function

--- server.js
+++ server.js
@@ -133,98 +133,104 @@ function addDepartment() {
 
 function addRole() {
     inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "What's the name of the role?",
-          name: "roleName"
-        },
-        {
-          type: "input",
-          message: "What is the salary for this role?",
-          name: "salaryTotal"
-        },
-        {
-          type: "input",
-          message: "What is the department id number?",
-          name: "deptID"
-        }
-      ])
-      .then(function(answer) {
-  
-  
-        connection.query("INSERT INTO employee_role (title, salary, department_id) VALUES (?, ?, ?)", [answer.roleName, answer.salaryTotal, answer.deptID], function(err, res) {
-          if (err) throw err;
-          console.table(res);
-          startPrompt();
+        .prompt([
+            {
+                type: "input",
+                message: "What's the name of the role?",
+                name: "roleName"
+            },
+            {
+                type: "input",
+                message: "What is the salary for this role?",
+                name: "salaryTotal"
+            },
+            {
+                type: "input",
+                message: "What is the department id number?",
+                name: "deptID"
+            }
+        ])
+        .then(function (answer) {
+
+
+            connection.query("INSERT INTO employee_role (title, salary, department_id) VALUES (?, ?, ?)", [answer.roleName, answer.salaryTotal, answer.deptID], function (err, res) {
+                if (err) throw err;
+                console.table(res);
+                startPrompt();
+            });
         });
-      });
-  }
+}
 
 
 
-  function addEmployee() {
+function addEmployee() {
     inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "What's the first name of the employee?",
-          name: "eeFirstName"
-        },
-        {
-          type: "input",
-          message: "What's the last name of the employee?",
-          name: "eeLastName"
-        },
-        {
-          type: "input",
-          message: "What is the employee's role id number?",
-          name: "roleID"
-        },
-        {
-          type: "input",
-          message: "What is the manager id number?",
-          name: "managerID"
-        }
-      ])
-      .then(function(answer) {
-  
-        
-        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answer.eeFirstName, answer.eeLastName, answer.roleID, answer.managerID], function(err, res) {
-          if (err) throw err;
-          console.table(res);
-          startPrompt();
+        .prompt([
+            {
+                type: "input",
+                message: "What's the first name of the employee?",
+                name: "eeFirstName"
+            },
+            {
+                type: "input",
+                message: "What's the last name of the employee?",
+                name: "eeLastName"
+            },
+            {
+                type: "input",
+                message: "What is the employee's role id number?",
+                name: "roleID"
+            },
+            {
+                type: "input",
+                message: "What is the manager id number?",
+                name: "managerID"
+            }
+        ])
+        .then(function (answer) {
+
+
+            connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answer.eeFirstName, answer.eeLastName, answer.roleID, answer.managerID], function (err, res) {
+                if (err) throw err;
+                console.table(res);
+                startPrompt();
+            });
         });
-      });
-  }
+}
 
 
 
-  function updateEmployee() {
+function updateEmployee() {
     inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "Which employee would you like to update?",
-          name: "eeUpdate"
-        },
-  
-        {
-          type: "input",
-          message: "What do you want to update to?",
-          name: "updateRole"
-        }
-      ])
-      .then(function(answer) {
-        // let sql = `INSERT INTO department (name) VALUES ("${answer.deptName}")`
-        //let sql = `'UPDATE employee SET role_id=${answer.updateRole} WHERE first_name= ${answer.eeUpdate}`;
-        //console.log(sql);
-  
-        connection.query('UPDATE employee SET role_id=? WHERE first_name= ?',[answer.updateRole, answer.eeUpdate],function(err, res) {
-          if (err) throw err;
-          console.table(res);
-          startPrompt();
-        });
-      });
-  }
+        .prompt([
+            {
+                type: "input",
+                message: "Which employee would you like to update?",
+                name: "eeUpdate"
+            },
 
+            {
+                type: "input",
+                message: "What do you want to update to?",
+                name: "updateRole"
+            }
+        ])
+        .then(function (answer) {
+            // let sql = `INSERT INTO department (name) VALUES ("${answer.deptName}")`
+            //let sql = `'UPDATE employee SET role_id=${answer.updateRole} WHERE first_name= ${answer.eeUpdate}`;
+            //console.log(sql);
+
+            connection.query('UPDATE employee SET role_id=? WHERE first_name= ?', [answer.updateRole, answer.eeUpdate], function (err, res) {
+                if (err) throw err;
+                console.table(res);
+                startPrompt();
+            });
+        });
+}
+
+
+//Quit the application
+function quit() {
+    connection.end();
+    process.exit();
+}
