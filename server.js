85477254f9a3763c0a4ac25f077732a9236d7251
Mike L <mike.levy.rva@gmail.com>
2022-08-14 20:36:54 -0400

    added 'viewDepartment' function

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const connection = mysql.createConnection({
     user: 'root',
     password: 'sqlRVA123',
     database: 'employee_db'
-  });
-  
+});
 
-  connection.connect(err => {
+
+connection.connect(err => {
     if (err) throw err;
     console.log('****************');
     console.log('Employee Manager');
@@ -20,53 +20,67 @@ const connection = mysql.createConnection({
 
     startPrompt()
 
-  });
+});
 
 
-  function startPrompt() {
+function startPrompt() {
     inquirer
-      .prompt({
-        type: "list",
-        choices: [
-          "View all departments",
-          "View all roles",
-          "View all employees",
-          "Add department",
-          "Add role",
-          "Add employee",
-          "Update employee role",
-          "Quit"
-        ],
-        message: "What would you like to do?",
-        name: "option"
-      })
-      .then(function(result) {
-        console.log("You entered: " + result.option);
-  
-        switch (result.option) {
-          case "View all departments":
-            viewDepartment();
-            break;
-          case "View all roles":
-            viewRoles();
-            break;
-          case "View all employees":
-            viewEmployees();
-            break;
-          case "Add department":
-            addDepartment();
-            break;
-          case "Add role":
-            addRole();
-            break;
-          case "Add employee":
-            addEmployee();
-            break;
-          case "Update employee role":
-            updateEmployee();
-            break;
-          default:
-            quit();
-        }
-      });
+        .prompt({
+            type: "list",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add department",
+                "Add role",
+                "Add employee",
+                "Update employee role",
+                "Quit"
+            ],
+            message: "What would you like to do?",
+            name: "option"
+        })
+        .then(function (result) {
+            console.log("You entered: " + result.option);
+
+            switch (result.option) {
+                case "View all departments":
+                    viewDepartment();
+                    break;
+                case "View all roles":
+                    viewRoles();
+                    break;
+                case "View all employees":
+                    viewEmployees();
+                    break;
+                case "Add department":
+                    addDepartment();
+                    break;
+                case "Add role":
+                    addRole();
+                    break;
+                case "Add employee":
+                    addEmployee();
+                    break;
+                case "Update employee role":
+                    updateEmployee();
+                    break;
+                default:
+                    quit();
+            }
+        });
+}
+
+
+//functions from above
+
+function viewDepartment() {
+    // select from the db
+    const sql = `SELECT * FROM department`;
+    connection.query(sql, function(err, res) {
+      if (err) throw err;
+      console.table(res);
+      startPrompt();
+    });
+    // show the result to the user (console.table)
   }
